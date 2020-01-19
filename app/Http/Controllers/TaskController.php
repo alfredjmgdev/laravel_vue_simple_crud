44c9f78bd1e19ccf55b8223b8baa779812c59e30
alfredjmg <alfredomujicagodoy@gmail.com>
2020-01-19 18:36:29 -0400

    uploading proyect

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = Task::orderBy('id', 'DESC')->paginate(2); 
+
+        return [
+            'pagination' => [
+                'total'         => $tasks->total(),
+                'current_page'  => $tasks->currentPage(),
+                'per_page'      => $tasks->perPage(),
+                'last_page'     => $tasks->lastPage(),
+                'from'          => $tasks->firstItem(),
+                'to'            => $tasks->lastPage(),
+            ],
+            'tasks' => $tasks 
+        ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'keep' => 'required'
+        ]);
+        
+        $key = new Task([
+            "keys" => $request["keep"]
+        ]);
+        $key->save();
+
+        return;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'keep'  => 'required'
+        ]);
+
+        $key = Task::find($id)->update([
+            "keys" => $request["keep"]
+        ]);
+
+        return;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+    }
+}
